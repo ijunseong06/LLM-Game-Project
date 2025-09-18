dd06dfed3b1c6c94cd677315fdfe9339f5194d2c
ijunseong06 <kingjuns2006@gmail.com>
2025-09-18 11:53:25 +0900

    feat: create session load logic

--- server/src/api/game_session.py
+++ server/src/api/game_session.py
@@ -14,3 +14,7 @@ def get_current_session(session : Session = Depends(get_session)):
 @router.post('/save')
 def save_session(session : Session = Depends(get_session), repo : SessionRepository = Depends(get_session_repo)):
     repo.save_session(session)
+
+@router.post('/load')
+def load_session(name_to_load : str, session : Session = Depends(get_session), repo : SessionRepository = Depends(get_session_repo)):
+    repo.load_session(name_to_load, session)
--- server/src/services/session_repository.py
+++ server/src/services/session_repository.py
@@ -1,3 +1,4 @@
+import json
 from pathlib import Path
 
 import paths
@@ -8,6 +9,8 @@ class SessionRepository:
         self.save_directory = paths.SAVE_PATH
 
     def save_session(self, session : Session):
+        if not (Path.exists(self.save_directory)):
+            self.save_directory.mkdir()
         if not (Path.exists(self.save_directory / f'{session.name}')):
             directory = self.save_directory / f'{session.name}'
             directory.mkdir()
@@ -16,3 +19,14 @@ class SessionRepository:
                 f.write(session.model_dump_json(indent=2))
         except Exception as e:
             print("error:" + str(e))
+
+    def load_session(self, session_name : str, session : Session):
+        try:
+            with open(self.save_directory / f'{session_name}' / 'session.json', 'r', encoding='utf-8') as f:
+                data = json.load(f)
+                session.name = data['name']
+                session.description = data['description']
+        except FileNotFoundError as e:
+            print(str(e))
+        except Exception as e:
+            print(str(e))
