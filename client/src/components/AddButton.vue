200b5f0da65de3915018a372ca48b0079f124326
ijunseong06 <kingjuns2006@gmail.com>
2025-09-22 23:24:39 +0900

    commit

---
+++ client/src/components/AddButton.vue
@@ -0,0 +1,18 @@
+<template>
+    <button class="add-button">
+        <AddIcon></AddIcon>
+    </button>
+</template>
+
+<style>
+.add-button {
+    background-color: rgb(19, 20, 34);
+    color: white;
+    border: none;
+    border-radius: 50%;
+}
+</style>
+
+<script setup>
+import AddIcon from '@/assets/icons/add-icon.svg';
+</script>
--- run_dev.bat
+++ run_dev.bat
@@ -2,4 +2,4 @@
 
 start "FastAPI Server" cmd /k "cd /d server && uvicorn app:app --reload"
 
-start "Flutter APP" cmd /k "cd /d client && flutter run"
+start "Flutter APP" cmd /k "cd /d client && npm run dev"
