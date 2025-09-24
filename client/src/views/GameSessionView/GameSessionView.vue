24ed9129b2844e783a17fdbe51f21df731168257
ijunseong06 <kingjuns2006@gmail.com>
2025-09-24 20:45:55 +0900

    commit

--- client/src/views/GameSessionView/GameSessionView.vue
+++ client/src/views/GameSessionView/GameSessionView.vue
@@ -1,17 +1,25 @@
 <script setup>
-import { ref } from "vue";
+import { onMounted, ref } from "vue";
 import axios from "axios";
 
-const session = ref(() => {
-  const response = axios.get('http://localhost:8000/session/get');
-  return response;
-});
-const sessionName = ref(session.value['name']);
+const session = ref({})
+const sessionName = ref('');
 
+onMounted(async () => {
+  try {
+    const response = await axios.get('http://localhost:8000/session/get');
+    session.value = response.data;
+    sessionName.value = session.value['name'];
+  }
+  catch (error) {
+    console.error("Failed to fetch session data:", error);
+  }
+});
 </script>
 
 <template>
   <v-app-bar>
+    <v-app-bar-nav-icon></v-app-bar-nav-icon>
     <v-app-bar-title>{{ sessionName }}</v-app-bar-title>
   </v-app-bar>
 </template>
