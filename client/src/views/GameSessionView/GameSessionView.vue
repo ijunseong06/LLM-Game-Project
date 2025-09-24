eba77332c7d0bc2f01c281511bfe56e1b94d4d70
ijunseong06 <kingjuns2006@gmail.com>
2025-09-24 11:38:45 +0900

    commit

--- client/src/views/GameSessionView/GameSessionView.vue
+++ client/src/views/GameSessionView/GameSessionView.vue
@@ -1,6 +1,17 @@
 <script setup>
+import { ref } from "vue";
+import axios from "axios";
+
+const session = ref(() => {
+  const response = axios.get('http://localhost:8000/session/get');
+  return response;
+});
+const sessionName = ref(session.value['name']);
+
 </script>
 
 <template>
-  <v-app-bar></v-app-bar>
+  <v-app-bar>
+    <v-app-bar-title>{{ sessionName }}</v-app-bar-title>
+  </v-app-bar>
 </template>
