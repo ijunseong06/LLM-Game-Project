c505abd7fde13541d534cdf50977cc8274a9dd4f
ijunseong06 <kingjuns2006@gmail.com>
2025-10-22 22:26:57 +0900

    commit

--- client/src/components/overlays/SessionSettingOverlay.vue
+++ client/src/components/overlays/SessionSettingOverlay.vue
@@ -9,6 +9,11 @@ const playerGender = ref('');
 const playerAge = ref(0);
 const playerRace = ref('');
 const playerAppearence = ref('');
+const playerPersonality = ref('');
+const playerBackground = ref('');
+const playerNote = ref('');
+
+const statsList = ref([''])
 
 const saveSettings = async () => {
   localStorage.setItem('playerName', playerName.value);
@@ -16,13 +21,19 @@ const saveSettings = async () => {
   localStorage.setItem('playerAge', playerAge.value);
   localStorage.setItem('playerRace', playerRace.value);
   localStorage.setItem('playerAppearence', playerAppearence.value);
+  localStorage.setItem('playerPersonality', playerPersonality.value);
+  localStorage.setItem('playerBackground', playerBackground.value);
+  localStorage.setItem('playerNote', playerNote.value);
   await axios.post('http://localhost:8000/session/save/player', null, {
     params: {
       name: localStorage.getItem('playerName'),
       gender: localStorage.getItem('playerGender'),
       age: localStorage.getItem('playerAge'),
       race: localStorage.getItem('playerRace'),
-      appearence: localStorage.getItem('playerAppearence')
+      appearence: localStorage.getItem('playerAppearence'),
+      personality: localStorage.getItem('playerPersonality'),
+      background: localStorage.getItem('playerBackground'),
+      note: localStorage.getItem('playerNote')
     }
   });
 };
@@ -54,6 +65,7 @@ onMounted(async () => {
       <v-sheet :width="256" class="border-e">
         <v-list nav dense>
           <v-list-item :title="$t('sessionSettingPlayerHeader')" @click="currentSetting = 'player'" :active="currentSetting === 'player'"></v-list-item>
+          <v-list-item :title="$t('sessionSettingGeneralHeader')" @click="currentSetting = 'general'" :active="currentSetting === 'general'"></v-list-item>
         </v-list>
       </v-sheet>
 
@@ -65,6 +77,12 @@ onMounted(async () => {
           <v-textarea :label="$t('sessionSettingPlayerAge')" variant="outlined" style="max-width: 400px;" v-model="playerAge" auto-grow rows="1"></v-textarea>
           <v-textarea :label="$t('sessionSettingPlayerRace')" variant="outlined" style="max-width: 400px;" v-model="playerRace" auto-grow rows="1"></v-textarea>
           <v-textarea :label="$t('sessionSettingPlayerAppearence')" variant="outlined" style="max-width: 400px;" v-model="playerAppearence" auto-grow rows="1"></v-textarea>
+          <v-textarea :label="$t('sessionSettingPlayerPersonality')" variant="outlined" style="max-width: 400px;" v-model="playerPersonality" auto-grow rows="1"></v-textarea>
+          <v-textarea :label="$t('sessionSettingPlayerBackground')" variant="outlined" style="max-width: 400px;" v-model="playerBackground" auto-grow rows="1"></v-textarea>
+          <v-textarea :label="$t('sessionSettingPlayerNote')" variant="outlined" style="max-width: 400px;" v-model="playerNote" auto-grow rows="1"></v-textarea>
+        </div>
+        <div v-if="currentSetting === 'general'">
+          <h3 class="mb-4">{{ $t('sessionSettingGeneralSubheader') }}</h3>
         </div>
         
         <div v-else>
